e67ca0f8584cb115be58545d19c07063c12adf5f
Jesús Castro <jesus.castro89@gmail.com>
2025-10-12 19:47:02 -0600

    Update

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -1,35 +1,15 @@
 public class Ejemplo {
 
     void main() {
-
-        int numero = Integer.parseInt(
-                IO.readln("Ingrese un número entero: "));
-        IO.println("%s x 1 = %s".formatted(numero, numero * 1));
-        IO.println("%s x 2 = %s".formatted(numero, numero * 2));
-        IO.println("%s x 3 = %s".formatted(numero, numero * 3));
-        IO.println("%s x 4 = %s".formatted(numero, numero * 4));
-        IO.println("%s x 5 = %s".formatted(numero, numero * 5));
-        IO.println("%s x 6 = %s".formatted(numero, numero * 6));
-        IO.println("%s x 7 = %s".formatted(numero, numero * 7));
-        IO.println("%s x 8 = %s".formatted(numero, numero * 8));
-        IO.println("%s x 9 = %s".formatted(numero, numero * 9));
-        IO.println("%s x 10 = %s".formatted(numero, numero * 10));
-
-        int i = 1;
-        while (i <= 10) {
-            IO.println(i);
-            i++;
-        }
-
-        int a = Integer.parseInt(
-                IO.readln("Ingrese un número entero: "));
-        int b = Integer.parseInt(
-                IO.readln("Ingrese otro número entero: "));
-
-        if (a % b == 0) {
-            IO.println("%s es múltiplo de %s".formatted(a, b));
-        } else {
-            IO.println("%s no es múltiplo de %s".formatted(a, b));
-        }
+        // Escribir
+        IO.println("Texto");
+        // Leer un entero
+        int n = Integer.parseInt(IO.readln("Número: "));
+        // Leer un real
+        double x = Double.parseDouble(IO.readln("Real: "));
+        // Leer una cadena
+        String s = IO.readln("Cadena: ");
+        // Mostrar variables
+        IO.println("n = " + n + ", x = " + x + ", s = " + s);
     }
 }
